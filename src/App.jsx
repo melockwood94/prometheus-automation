586c52a5ec0605c5e6615399c4fc274cd548746a
Matt Lockwood <melockwood94@gmail.com>
2023-11-16 20:11:58 -0500

    Added initial contact form.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Header from './Components/Header/Header'
 import Home from './Pages/Home/Home'
+import Contact from './Pages/Contact/Contact'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
 
---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+function Contact() {
+    return (
+        <>
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Contact Us</h5>
+                <p className="card-text">Let us know how we can help.</p>
+                <form action="">
+                    <div className="form-group">
+                        <label htmlFor="contact-name">Name</label>
+                        <input type="text" id="contact-name" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="contact-email">Email address</label>
+                        <input type="email" id="contact-email" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="contact-notes">What would you like to chat about?</label>
+                        <textarea name="contact-notes" id="contact-notes" cols="30" rows="10" className="form-control"></textarea>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">Send</button>
+                </form>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Contact;
